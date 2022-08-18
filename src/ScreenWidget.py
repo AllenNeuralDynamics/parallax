af170025d1534a3522d67747a5c6b07137834fba
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2022-08-18 10:55:27 -0700

    ScreenWidget: separate frame and overlay, use crosshairs

--- src/ScreenWidget.py
+++ src/ScreenWidget.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout, QPushButton, QMenu
-from PyQt5.QtGui import QPainter, QPixmap, QImage, QColor, qRgb
+from PyQt5.QtGui import QPainter, QPixmap, QImage, QColor, qRgb, qRgba
 from PyQt5.QtCore import Qt, pyqtSignal, QRect
 
 import numpy as np
@@ -23,7 +23,12 @@ class ScreenWidget(QLabel):
         self.setMinimumSize(WIDTH_SCREEN, HEIGHT_SCREEN)
         self.setMaximumSize(WIDTH_SCREEN, HEIGHT_SCREEN)
 
-        self.qimage = QImage(WIDTH_FRAME, HEIGHT_FRAME, QImage.Format_RGB32)
+        self.pixmap = QPixmap(WIDTH_SCREEN, HEIGHT_SCREEN)
+        self.painter = QPainter(self.pixmap)
+
+        self.frame = QImage(WIDTH_FRAME, HEIGHT_FRAME, QImage.Format_RGB32)
+        self.overlay = QImage(WIDTH_FRAME, HEIGHT_FRAME, QImage.Format_ARGB32)
+        self.overlay.fill(qRgba(0,0,0,0))
         self.display()
 
         self.clearSelected()
@@ -48,7 +53,8 @@ class ScreenWidget(QLabel):
         rgbData = np.zeros((HEIGHT_FRAME,WIDTH_FRAME,4), dtype=np.uint8)
         for i in range(3):
             rgbData[:,:,i] = self.data
-        self.qimage = QImage(rgbData, rgbData.shape[1], rgbData.shape[0], QImage.Format_RGB32)
+        self.qimage = QImage(rgbData, rgbData.shape[1], rgbData.shape[0],
+                                QImage.Format_RGB32)
 
         self.xsel = False
         self.ysel = False
@@ -63,25 +69,27 @@ class ScreenWidget(QLabel):
             self.display_scaled()
 
     def display_scaled(self):
-
-        pixmap = QPixmap(self.qimage.scaled(WIDTH_SCREEN, HEIGHT_SCREEN, Qt.IgnoreAspectRatio,
-                            Qt.SmoothTransformation))
-        self.setPixmap(pixmap)
+        self.painter.drawImage(0,0, self.frame.scaled(WS, HS, Qt.IgnoreAspectRatio,
+                                Qt.SmoothTransformation))
+        self.painter.drawImage(0,0, self.overlay.scaled(WS, HS, Qt.IgnoreAspectRatio,
+                                Qt.SmoothTransformation))
+        self.setPixmap(self.pixmap)
         self.update()
 
     def display_zoom(self):
-
-        rect = QRect(self.xc-WS/2, self.yc-HS/2, WS, HS)
-        self.qimage_zoom = self.qimage.copy(rect)
-        pixmap = QPixmap(self.qimage_zoom)
-        self.setPixmap(pixmap)
+        rect = QRect(int(self.xc-WS/2), int(self.yc-HS/2), WS, HS)
+        self.painter.drawImage(0,0, self.frame.copy(rect))
+        self.painter.drawImage(0,0, self.overlay.copy(rect))
+        self.setPixmap(self.pixmap)
         self.update()
 
     def updatePixel(self, x, y): 
-
-        for i in range(x-8, x+8):
-            for j in range(y-8, y+8):
-                self.qimage.setPixel(i, j, qRgb(255, 0, 0)) # red
+        self.overlay.fill(qRgba(0,0,0,0))
+        # ^ this could be faster if we just qfill the known previous pixel
+        for i in range(x-8, x+9):
+            for j in range(y-8, y+9):
+                if (i==x) or (j==y): # crosshairs
+                    self.overlay.setPixel(i, j, qRgba(255, 0, 0, 255)) # green
         self.display()
 
     def setCamera(self, camera):
