269b1157ce3506f28683c7fcf010da59f4cf8dde
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2022-11-29 23:46:38 -0800

    remove unused library functions
    - helper.get_intrinsics_from_checkerboard()
    - lib.get_intrinsics_from_checkerboards()
    - lib.get_points_from_csv()

--- parallax/lib.py
+++ parallax/lib.py
@@ -24,65 +24,6 @@ DIST_GUESS =  np.array([[0., 0., 0., 0., 0.]], dtype=np.float32)
 INTRINSICS_USE_INITIAL_GUESS = False
 
 
-def get_intrinsics_from_checkerboards(images_folder):
-    images_names = glob.glob(images_folder)
-    images = []
-    for imname in images_names:
-        im = cv.imread(imname, 1)
-        images.append(im)
- 
-    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
- 
-    imgpoints = [] # 2d points in image plane.
-    objpoints = [] # 3d point in real world space
- 
-    nsuccess = 0
-    for i,image in enumerate(images):
-
-        gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-        gray_small = cv.pyrDown(gray)       # scale down by a factor of 2
-        gray_small = cv.pyrDown(gray_small) # factor of 4
-
-        # find the checkerboard corners roughly
-        ret, corners_small = cv.findChessboardCorners(gray_small, (CB_ROWS, CB_COLS), None)
- 
-        if ret == True:
-            corners = corners_small * 4
-            #Convolution size used to improve corner detection. Don't make this too large.
-            conv_size = (11, 11)
-            corners = cv.cornerSubPix(gray, corners, conv_size, (-1, -1), criteria)
-
-            # optional: inspect corners visually
-            cv.drawChessboardCorners(image, (CB_ROWS,CB_COLS), corners, ret)
-            cv.imshow('img', image)
-            while (cv.waitKey(0) != 113):
-                pass
-
-            objpoints.append(OBJPOINTS_CB)
-            imgpoints.append(corners)
-            nsuccess += 1
-        else:
-            print('corners not found')
- 
-    if INTRINSICS_USE_INITIAL_GUESS:
-        rmse, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, (IMG_WIDTH, IMG_HEIGHT),
-                                            MTX_GUESS, DIST_GUESS, flags=cv.CALIB_USE_INTRINSIC_GUESS)
-    else:
-        rmse, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, (IMG_WIDTH, IMG_HEIGHT),
-                                            None, None)
-    
-    # optional: print results
-    """
-    print('---')
-    print('found %d good checkerboard images in %s' % (nsuccess, images_folder))
-    print('rmse:', rmse)
-    print('camera matrix:\n', mtx)
-    print('distortion coeffs:', dist)
-    print('---')
-    """
-
-    return mtx, dist
- 
 def undistort_image_points(img_points, mtx, dist):
     img_points_corrected_normalized = cv.undistortPoints(img_points, mtx, dist)
     fx = mtx[0,0]
@@ -96,21 +37,6 @@ def undistort_image_points(img_points, mtx, dist):
         y = y * fy + cy
         img_points_corrected.append(np.array([x,y]))
     return np.array([img_points_corrected], dtype=np.float32)
-
-def get_points_from_csv(filename):
-    points = np.genfromtxt(filename, delimiter=',')
-    obj_points = []
-    img_points1 = []
-    img_points2 = []
-    for point in points:
-        ox, oy, oz, x1, y1, x2, y2 = point
-        obj_points.append([ox,oy,oz])
-        img_points1.append([x1,y1])
-        img_points2.append([x2,y2])
-    obj_points = np.array([obj_points], dtype=np.float32)
-    img_points1 = np.array([img_points1], dtype=np.float32)
-    img_points2 = np.array([img_points2], dtype=np.float32)
-    return obj_points, img_points1, img_points2
 
 def get_projection_matrix(mtx, r, t):
     R, jacobian = cv.Rodrigues(r)
