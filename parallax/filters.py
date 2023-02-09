627e2d080fb789f1f62d5692596aac6b15597fde
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2023-02-09 14:46:23 -0800

    AlphaBetaFilter.control_panel: new layout

--- parallax/filters.py
+++ parallax/filters.py
@@ -1,6 +1,6 @@
 import cv2
 
-from PyQt5.QtWidgets import QWidget, QHBoxLayout, QSlider
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QSlider
 from PyQt5.QtCore import pyqtSignal, Qt
 
 class NoFilter():
@@ -35,12 +35,12 @@ class AlphaBetaFilter():
 
     def launch_control_panel(self):
         self.control_panel = QWidget()
-        layout = QHBoxLayout()
-        self.brightness_slider = QSlider(Qt.Vertical)
+        layout = QVBoxLayout()
+        self.brightness_slider = QSlider(Qt.Horizontal)
         self.brightness_slider.setValue(50)
         self.brightness_slider.setToolTip('Brightness')
         self.brightness_slider.sliderMoved.connect(self.set_beta)
-        self.contrast_slider = QSlider(Qt.Vertical)
+        self.contrast_slider = QSlider(Qt.Horizontal)
         self.contrast_slider.setValue(25)
         self.contrast_slider.setToolTip('Contrast')
         self.contrast_slider.sliderMoved.connect(self.set_alpha)
@@ -48,5 +48,6 @@ class AlphaBetaFilter():
         layout.addWidget(self.contrast_slider)
         self.control_panel.setLayout(layout)
         self.control_panel.setWindowTitle('Brightness and Contrast')
+        self.control_panel.setMinimumWidth(300)
         self.control_panel.show()
 
