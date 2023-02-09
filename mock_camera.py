1bc6fa1ab1e68a7f2607fba698fdd5d0bedceb3b
Luke Campagnola <luke.campagnola@gmail.com>
2023-02-09 03:02:50 -0800

    add radial distortion

--- mock_camera.py
+++ mock_camera.py
@@ -1,4 +1,5 @@
 import pyqtgraph as pg
+import cv2 as cv
 import numpy as np
 import coorx
 from parallax.calibration import CameraTransform, StereoCameraTransform
@@ -104,14 +105,35 @@ def frustum(left, right, bottom, top, znear, zfar):
     return M
 
 
+class RadialDistortionTransform(coorx.BaseTransform):
+    def __init__(self, k=(0, 0, 0)):
+        super().__init__(dims=(3, 3))
+        self.k = k
+
+    def set_k(self, k):
+        self.k = k
+        self._update()
+
+    def _map(self, arr):
+        r = np.linalg.norm(arr, axis=1)
+        dist = (1 + self.k[0] * r**2 + self.k[1] * r**4 + self.k[2] * r**6)
+        out = np.empty_like(arr)
+        # distort x,y
+        out[:, :2] = arr[:, :2] * dist[:, None]
+        # leave other axes unchanged
+        out[:, 2:] = arr[:, 2:]
+        return out
+
+
 class CameraTransform(coorx.CompositeTransform):
     def __init__(self):
         self.view = coorx.SRT3DTransform()
         self.proj = PerspectiveTransform()
+        self.dist = RadialDistortionTransform()
         self.screen = coorx.STTransform(dims=(3, 3))
-        super().__init__([self.view, self.proj, self.screen])
+        super().__init__([self.view, self.proj, self.dist, self.screen])
 
-    def set_camera(self, center, look, fov, screen_size, up=(0, 0, 1)):
+    def set_camera(self, center, look, fov, screen_size, up=(0, 0, 1), distortion=(0, 0, 0)):
         center = np.asarray(center)
         look = np.asarray(look)
         up = np.asarray(up)
@@ -136,6 +158,7 @@ class CameraTransform(coorx.CompositeTransform):
             [[0, screen_size[1], 0], [screen_size[0], 0, 1]]
         )
 
+        self.dist.set_k(distortion)
 
 
 class GraphicsItem:
@@ -247,7 +270,7 @@ class GraphicsView3D(pg.GraphicsView):
     def __init__(self, **kwds):
         self.camera_tr = CameraTransform()
         self.press_event = None
-        self.camera_params = {'look': [0, 0, 0], 'pitch': 30, 'yaw': 0, 'dist': 10, 'fov': 45}
+        self.camera_params = {'look': [0, 0, 0], 'pitch': 30, 'yaw': 0, 'dist': 10, 'fov': 45, 'distortion': (0, 0, 0)}
         super().__init__(**kwds)
         self.setRenderHint(pg.QtGui.QPainter.Antialiasing)
         self.set_camera(look=[0, 0, 0], pitch=30, yaw=0, dist=10, fov=45)
@@ -269,7 +292,7 @@ class GraphicsView3D(pg.GraphicsView):
             hdist * np.sin(yaw),
             p['dist'] * np.sin(pitch)
         ])
-        self.camera_tr.set_camera(center=cam_pos, look=look, fov=p['fov'], screen_size=[self.width(), self.height()])
+        self.camera_tr.set_camera(center=cam_pos, look=look, fov=p['fov'], screen_size=[self.width(), self.height()], distortion=p['distortion'])
 
     def mousePressEvent(self, ev):
         self.press_event = ev
@@ -289,6 +312,10 @@ class GraphicsView3D(pg.GraphicsView):
     def mouseReleaseEvent(self, ev):
         self.press_event = None
 
+    def wheelEvent(self, event):
+        self.camera_params['dist'] *= 1.01**event.angleDelta().y()
+        self.update_camera()
+
     def resizeEvent(self, ev):
         super().resizeEvent(ev)
         self.update_camera()
@@ -300,11 +327,13 @@ class GraphicsView3D(pg.GraphicsView):
 def find_checker_corners(img, board_shape, show=False):
     """https://docs.opencv.org/4.x/dc/dbb/tutorial_py_calibration.html
     """
-    import cv2 as cv
+    if show:
+        view = pg.image(img)
+
     criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-    objp = np.zeros((board_shape[0] * board_shape[1], 3))
+    objp = np.zeros((board_shape[0] * board_shape[1], 3), dtype='float32')
     objp[:, :2] = np.mgrid[0:board_shape[0], 0:board_shape[1]].T.reshape(-1, 2)
 
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -312,19 +341,57 @@ def find_checker_corners(img, board_shape, show=False):
     # Find the chess board corners
     ret, corners = cv.findChessboardCorners(gray, board_shape, None)
     if not ret:
-        print(ret, corners)
-        return None
+        return None, None
 
     # If found, add object points, image points (after refining them)
     imgp = cv.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
 
     if show:
-        v = pg.image(img)
         plt = pg.ScatterPlotItem(x=imgp[:, 0, 1], y=imgp[:, 0, 0])
-        v.view.addItem(plt)
+        view.view.addItem(plt)
 
     return objp, imgp
 
+
+def generate_calibration_data(view, size):
+    imgp = []
+    objp = []
+    cam = view.camera_params.copy()
+    for i in range(size*4):
+        pitch = np.random.uniform(45, 89)
+        yaw = np.random.uniform(0, 360)
+        distance = np.random.uniform(5, 15)
+        view.set_camera(pitch=pitch, yaw=yaw, dist=distance)
+        op,ip = find_checker_corners(view.get_array(), (4, 4))
+        if op is None:
+            continue
+        objp.append(op)
+        imgp.append(ip)
+        if len(imgp) >= size:
+            break
+        app.processEvents()
+    view.set_camera(**cam)
+    return objp, imgp
+
+
+def calibrate_camera(view, size=40):
+    objp, imgp = generate_calibration_data(view, size=size)
+    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objp, imgp, (view.width(), view.height()), None, None)
+    return ret, mtx, dist, rvecs, tvecs
+
+
+def undistort_image(img, mtx, dist):
+    h, w = img.shape[:2]
+    new_camera_mtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+
+    # undistort
+    mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, new_camera_mtx, (w,h), 5)
+    dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
+
+    # crop the image
+    x, y, w, h = roi
+    dst = dst[y:y+h, x:x+w]
+    return dst
 
 
 if __name__ == '__main__':
@@ -337,6 +404,8 @@ if __name__ == '__main__':
     win.setBackground(pg.mkColor(128, 128, 128))
     win.resize(*screen_size)
     win.show()
+
+    win.set_camera(distortion=(-0.03, 0, 0))
 
     checkers = CheckerBoard(win, size=5, colors=[0.1, 0.9])
     checkers.transform.set_params(offset=[-2.5, -2.5, -1])
@@ -359,8 +428,7 @@ if __name__ == '__main__':
     tr.rotate(30, [1, 0, 0])
     tr.rotate(45, [0, 0, 1]) 
 
-    # checkers.transform.set_from_affine(tr)
-
-
-    # checkers2 = CheckerBoard(win, size=5, colors=[(0, 0, 0, 40), (255, 255, 255, 40)])
-    # checkers2.transform = tr
+    def test():
+        ret, mtx, dist, rvecs, tvecs = calibrate_camera(win)
+        print(dist)
+        pg.image(undistort_image(win.get_array(), mtx, dist))
