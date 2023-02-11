3eae984a0ee409d5fa375d3db73b648832bf95e8
Luke Campagnola <luke.campagnola@gmail.com>
2023-02-11 12:12:40 -0800

    Don't change CWD if possible -- this is global

--- parallax/__init__.py
+++ parallax/__init__.py
@@ -7,5 +7,5 @@ __version__ = "0.5.1"
 # allow multiple OpenMP instances
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
-# change workdir to package root
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
+# # change workdir to package root
+# os.chdir(os.path.dirname(os.path.realpath(__file__)))
