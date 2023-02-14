9486cd04d60732f64acea465e92f673c8d8b2a13
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2023-02-14 12:37:37 -0800

    add Difference Filter

--- parallax/filters.py
+++ parallax/filters.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QSlider
 from PyQt5.QtCore import pyqtSignal, Qt
@@ -48,6 +49,39 @@ class AlphaBetaFilter():
         layout.addWidget(self.contrast_slider)
         self.control_panel.setLayout(layout)
         self.control_panel.setWindowTitle('Brightness and Contrast')
+        self.control_panel.setMinimumWidth(300)
+        self.control_panel.show()
+
+class DifferenceFilter():
+
+    name = 'Difference'
+
+    def __init__(self):
+        self.buff = None
+        self.alpha = 1.0
+
+    def process(self, frame):
+        if self.buff is not None:
+            buff_scaled = np.array(self.alpha * self.buff, dtype=np.uint8)
+            result = frame - buff_scaled
+        else:
+            result = frame
+        self.buff = frame
+        return result
+
+    def set_alpha(self, value):
+        self.alpha = value / 99.
+
+    def launch_control_panel(self):
+        self.control_panel = QWidget()
+        layout = QVBoxLayout()
+        self.mix_slider = QSlider(Qt.Horizontal)
+        self.mix_slider.setValue(99)
+        self.mix_slider.setToolTip('Mix')
+        self.mix_slider.sliderMoved.connect(self.set_alpha)
+        layout.addWidget(self.mix_slider)
+        self.control_panel.setLayout(layout)
+        self.control_panel.setWindowTitle('Difference Filter')
         self.control_panel.setMinimumWidth(300)
         self.control_panel.show()
 
