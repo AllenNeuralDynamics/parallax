6eeb275f7263e2b3df76de1e6701be39df114234
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2023-02-15 23:43:35 -0800

    implement saving/loading of templates

--- parallax/template_tool.py
+++ parallax/template_tool.py
@@ -1,7 +1,11 @@
-from PyQt5.QtWidgets import QPushButton, QLabel, QWidget, QFrame, QInputDialog, QComboBox
-from PyQt5.QtWidgets import QVBoxLayout
-from PyQt5.QtCore import pyqtSignal, QTimer
+from PyQt5.QtWidgets import QPushButton, QLabel, QWidget
+from PyQt5.QtWidgets import QVBoxLayout, QFileDialog
+from PyQt5.QtCore import pyqtSignal, QTimer, Qt
+
 import numpy as np
+import time
+import datetime
+import os
 
 from .screen_widget import ScreenWidget
 
@@ -12,18 +16,17 @@ class TemplateTool(QWidget):
 
     def __init__(self, model):
         QWidget.__init__(self, parent=None)
+        self.setAttribute(Qt.WA_DeleteOnClose)
         self.model = model
 
-        self.button1 = QPushButton('Button 1')
         self.screen = ScreenWidget(model=self.model)
         self.screen.update_camera_menu()
-        self.button2 = QPushButton('Save Template')
-        self.button2.clicked.connect(self.save_template)
+        self.save_button = QPushButton('Save Template')
+        self.save_button.clicked.connect(self.save)
 
         self.layout = QVBoxLayout()
-        self.layout.addWidget(self.button1)
         self.layout.addWidget(self.screen)
-        self.layout.addWidget(self.button2)
+        self.layout.addWidget(self.save_button)
         self.setLayout(self.layout)
 
         self.setWindowTitle('Generate Template Tool')
@@ -32,17 +35,24 @@ class TemplateTool(QWidget):
         self.refresh_timer.timeout.connect(self.screen.refresh)
         self.refresh_timer.start(250)
 
-    def save_template(self):
+    def save(self):
         if self.screen.click_target.isVisible():
-            pos = self.screen.click_target.pos()
-            x,y = pos.x(), pos.y()
-            print(x,y)
-            pm = self.screen.image_item.getPixmap()
-            template_pm = pm.copy(x-RAD,y-RAD, 2*RAD, 2*RAD)
-            template_im = template_pm.toImage()
-            print('format = ', template_im.format())
-            w = h = 2*RAD
-            s = template_im.bits().asstring(w * h * 4)
-            arr = np.fromstring(s, dtype=np.uint8).reshape((h, w, 4)) 
-            np.save('template.npy', arr[:,:,:3])
+            ts = time.time()
+            dt = datetime.datetime.fromtimestamp(ts)
+            suggested_basename = 'template_%04d%02d%02d-%02d%02d%02d.npy' % (dt.year,
+                                            dt.month, dt.day, dt.hour, dt.minute, dt.second)
+            suggested_filename = os.path.join(os.getcwd(), suggested_basename)
+            filename = QFileDialog.getSaveFileName(self, 'Save template',
+                                                    suggested_filename,
+                                                    'Numpy files (*.npy)')[0]
+            if filename:
+                pos = self.screen.click_target.pos()
+                x,y = pos.x(), pos.y()
+                pm = self.screen.image_item.getPixmap()
+                template_pm = pm.copy(x-RAD,y-RAD, 2*RAD, 2*RAD)
+                template_im = template_pm.toImage()
+                w = h = 2*RAD
+                s = template_im.bits().asstring(w * h * 4)
+                arr = np.fromstring(s, dtype=np.uint8).reshape((h, w, 4)) 
+                np.save(filename, arr[:,:,:3])
 
