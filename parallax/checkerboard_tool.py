55a02e92e8fb6d3294521995f7caa8f0e696a94b
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2023-07-20 13:05:26 -0700

    CheckerboardToolMono: fix bugs, save cal

--- parallax/checkerboard_tool.py
+++ parallax/checkerboard_tool.py
@@ -8,11 +8,12 @@ import numpy as np
 import time
 import datetime
 import os
+import pickle
 
 from . import get_image_file, data_dir
 from .screen_widget import ScreenWidget
 from .filters import CheckerboardFilter
-from .calibration import Calibration
+from .calibration import Calibration, CalibrationMono
 
 CB_ROWS = 19 #number of checkerboard rows.
 CB_COLS = 19 #number of checkerboard columns.
@@ -34,24 +35,29 @@ class CheckerboardToolMono(QWidget):
         self.model = model
 
         self.lscreen = ScreenWidget(model=self.model)
-        self.save_button = QPushButton('Save Corners')
-        self.save_button.clicked.connect(self.saveCorners)
-        self.export_button = QPushButton('Export Corners (None)')
-        self.export_button.clicked.connect(self.exportCorners)
-        self.import_button = QPushButton('Import Corners')
-        self.import_button.clicked.connect(self.import_corners)
-        self.generate_button = QPushButton('Generate Calibration from Corners')
-        self.generate_button.clicked.connect(self.generate)
+        self.grab_button = QPushButton('Grab Corners')
+        self.grab_button.clicked.connect(self.grab_corners)
+        self.calibrate_button = QPushButton('Calibrate')
+        self.calibrate_button.setEnabled(False)
+        self.calibrate_button.clicked.connect(self.calibrate)
+        self.save_corners_button = QPushButton('Save Corners (None)')
+        self.save_corners_button.clicked.connect(self.save_corners)
+        self.load_corners_button = QPushButton('Load Corners')
+        self.load_corners_button.clicked.connect(self.load_corners)
+        self.save_cal_button = QPushButton('Save Calibration')
+        self.save_cal_button.setEnabled(False)
+        self.save_cal_button.clicked.connect(self.save_cal)
 
         self.screens_layout = QHBoxLayout()
         self.screens_layout.addWidget(self.lscreen)
 
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.lscreen)
-        self.layout.addWidget(self.save_button)
-        self.layout.addWidget(self.export_button)
-        self.layout.addWidget(self.import_button)
-        self.layout.addWidget(self.generate_button)
+        self.layout.addWidget(self.grab_button)
+        self.layout.addWidget(self.calibrate_button)
+        self.layout.addWidget(self.save_corners_button)
+        self.layout.addWidget(self.load_corners_button)
+        self.layout.addWidget(self.save_cal_button)
         self.setLayout(self.layout)
 
         self.setWindowTitle('Checkerboard Calibration Tool')
@@ -66,39 +72,56 @@ class CheckerboardToolMono(QWidget):
 
         self.last_cal = None
 
-    def saveCorners(self):
+    def save_cal(self):
+        if (self.last_cal is not None):
+            self.model.add_calibration(self.last_cal)
+            self.cal_generated.emit()
+            suggested_filename = os.path.join(data_dir, self.last_cal.name + '.pkl')
+            filename = QFileDialog.getSaveFileName(self, 'Save calibration file',
+                                                    suggested_filename,
+                                                    'Pickle files (*.pkl)')[0]
+            if filename:
+                with open(filename, 'wb') as f:
+                    pickle.dump(self.last_cal, f)
+                self.msg_posted.emit('Saved calibration %s to: %s' % (self.last_cal.name, filename))
+
+
+    def grab_corners(self):
         lfilter = self.lscreen.filter
         if isinstance(lfilter, CheckerboardFilter):
             if (lfilter.worker.corners is not None):
                 self.lipts.append(lfilter.worker.corners)
                 self.opts.append(OBJPOINTS_CB)
+                if len(self.opts) >= 2:
+                    self.calibrate_button.setEnabled(True)
                 self.update_text()
 
     def update_text(self):
-        self.export_button.setText('Export Corners (%d)' % len(self.opts))
+        self.save_corners_button.setText('Export Corners (%d)' % len(self.opts))
 
-    def exportCorners(self):
+    def save_corners(self):
         ts = time.time()
         dt = datetime.datetime.fromtimestamp(ts)
-        suggested_basename = 'corners_%04d%02d%02d-%02d%02d%02d.npz' % (dt.year,
+        suggested_basename = 'corners_mono_%04d%02d%02d-%02d%02d%02d.npz' % (dt.year,
                                         dt.month, dt.day, dt.hour, dt.minute, dt.second)
         suggested_filename = os.path.join(data_dir, suggested_basename)
-        filename = QFileDialog.getSaveFileName(self, 'Save corners',
+        filename = QFileDialog.getSaveFileName(self, 'Save Corners File',
                                                 suggested_filename,
                                                 'Numpy files (*.npz)')[0]
         if filename:
             np.savez(filename, opts=self.opts, lipts=self.lipts)
             self.msg_posted.emit('Exported corners to %s' % filename)
 
-    def generate(self):
-        cal = CalibrationMono('checkerCal', 'checkerboard') # temp
+    def calibrate(self):
+        cal = CalibrationMono('checkerCalMono', 'checkerboard') # temp
         opts = np.array(self.opts, dtype=np.float32)
         lipts = np.array(self.lipts, dtype=np.float32).squeeze()
         cal.calibrate(lipts, opts)
         if (self.last_cal is not None):
-            dparams = self.calculate_last_change()
-            self.msg_posted.emit('Last change = %s' % np.array2string(dparams))
+            dparams = self.calculate_last_change(cal)
+            self.msg_posted.emit('Largest fractional change = %.2E' % np.max(np.abs(dparams)))
         self.last_cal = cal
+        self.save_cal_button.setEnabled(True)
 
     def calculate_last_change(self, cal):
         dparams = np.zeros(9)
@@ -106,14 +129,14 @@ class CheckerboardToolMono(QWidget):
         dparams[1] = (cal.mtx1[1,1] - self.last_cal.mtx1[1,1]) / self.last_cal.mtx1[1,1] # fy
         dparams[2] = (cal.mtx1[0,2] - self.last_cal.mtx1[0,2]) / self.last_cal.mtx1[0,2] # cx
         dparams[3] = (cal.mtx1[1,2] - self.last_cal.mtx1[1,2]) / self.last_cal.mtx1[1,2] # cy
-        dparams[4] = (cal.dist1[0] - self.last_cal.dist1[0]) / self.last_cal.dist1[0] # k1
-        dparams[5] = (cal.dist1[1] - self.last_cal.dist1[1]) / self.last_cal.dist1[1] # k2
-        dparams[6] = (cal.dist1[2] - self.last_cal.dist1[2]) / self.last_cal.dist1[2] # p1
-        dparams[7] = (cal.dist1[3] - self.last_cal.dist1[3]) / self.last_cal.dist1[3] # p2
-        dparams[8] = (cal.dist1[4] - self.last_cal.dist1[4]) / self.last_cal.dist1[4] # k3
+        dparams[4] = (cal.dist1[0,0] - self.last_cal.dist1[0,0]) / self.last_cal.dist1[0,0] # k1
+        dparams[5] = (cal.dist1[0,1] - self.last_cal.dist1[0,1]) / self.last_cal.dist1[0,1] # k2
+        dparams[6] = (cal.dist1[0,2] - self.last_cal.dist1[0,2]) / self.last_cal.dist1[0,2] # p1
+        dparams[7] = (cal.dist1[0,3] - self.last_cal.dist1[0,3]) / self.last_cal.dist1[0,3] # p2
+        dparams[8] = (cal.dist1[0,4] - self.last_cal.dist1[0,4]) / self.last_cal.dist1[0,4] # k3
         return dparams
 
-    def import_corners(self):
+    def load_corners(self):
         filename = QFileDialog.getOpenFileName(self, 'Load corners file', data_dir,
                                                     'Numpy files (*.npz)')[0]
         corners = np.load(filename)
@@ -134,7 +157,7 @@ class CheckerboardToolStereo(QWidget):
         self.lscreen = ScreenWidget(model=self.model)
         self.rscreen = ScreenWidget(model=self.model)
         self.save_button = QPushButton('Save Corners')
-        self.save_button.clicked.connect(self.saveCorners)
+        self.save_button.clicked.connect(self.save_corners)
         self.export_button = QPushButton('Export Corners (None)')
         self.export_button.clicked.connect(self.exportCorners)
         self.import_button = QPushButton('Import Corners')
@@ -166,7 +189,7 @@ class CheckerboardToolStereo(QWidget):
         self.lipts = [] # left image points
         self.ripts = [] # right image points
 
-    def saveCorners(self):
+    def save_corners(self):
         lfilter = self.lscreen.filter
         rfilter = self.rscreen.filter
         if isinstance(lfilter, CheckerboardFilter) and \
