1ba4939847b6e08bb72d58eec98897a1bbfcd6f9
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2023-07-26 12:13:44 -0700

    implement autotraining as background thread

--- parallax/__init__.py
+++ parallax/__init__.py
@@ -9,9 +9,14 @@ os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 package_dir = os.path.dirname(__file__)
 image_dir = os.path.join(os.path.dirname(package_dir), 'img')
 data_dir = os.path.join(os.path.dirname(package_dir), 'data')
+training_dir = os.path.join(os.path.dirname(package_dir), 'training')
+training_file = os.path.join(training_dir, 'metadata.csv')
 
 if not os.path.exists(data_dir):
     os.makedirs(data_dir)
+
+if not os.path.exists(training_dir):
+    os.makedirs(training_dir)
 
 def get_image_file(basename):
     return os.path.join(image_dir, basename)
--- parallax/main_window.py
+++ parallax/main_window.py
@@ -28,6 +28,7 @@ from .point_bank import PointBank
 from .ruler import Ruler
 from .camera import VideoSource
 from .preferences import PreferencesWindow
+from .helper import uid8
 
 
 class MainWindow(QMainWindow):
@@ -283,10 +284,26 @@ class MainWidget(QWidget):
                 self.cal_panel.register_corr_points_cal()
             if self.model.accutest_in_progress:
                 self.model.register_corr_points_accutest()
+            if self.model.prefs.train_c:
+                self.save_training_data()
         elif e.key() == Qt.Key_Escape:
             self.model.halt_all_stages()
         elif e.key() == Qt.Key_T:
             self.cal_panel.triangulate()
+            if self.model.prefs.train_t:
+                self.save_training_data()
+
+    def save_training_data(self):
+        if self.model.prefs.train_left:
+            if self.lscreen.camera is not None:
+                frame = self.lscreen.camera.get_last_image_data()
+                tag = 'left_%s_%s' % (self.lscreen.camera.name(), uid8())
+                self.model.save_training_data(self.model.lcorr, frame, tag)
+        if self.model.prefs.train_right:
+            if self.rscreen.camera is not None:
+                frame = self.rscreen.camera.get_last_image_data()
+                tag = 'right_%s_%s' % (self.rscreen.camera.name(), uid8())
+                self.model.save_training_data(self.model.rcorr, frame, tag)
 
     def refresh(self):
         self.lscreen.refresh()
