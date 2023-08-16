b5a398470d66e1c1ac003d1de5bc8c6854e80c87
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2023-08-16 13:11:39 -0700

    add triangulate_pose for mono

--- parallax/calibration.py
+++ parallax/calibration.py
@@ -14,6 +14,7 @@ idist = np.array([[ 0e+00, 0e+00, 0e+00, 0e+00, 0e+00 ]],
 
 CRIT = (cv2.TERM_CRITERIA_EPS, 0, 1e-8)
 
+
 class Calibration:
 
     def __init__(self, name, cs):
@@ -42,9 +43,9 @@ class Calibration:
         self.set_initial_intrinsics(imtx, imtx, idist, idist)
 
     def triangulate(self, lcorr, rcorr):
-        return self.triangulate_proj(lcorr, rcorr, self.projs1[-1], self.projs2[-1])
+        return self.triangulate_pose(lcorr, rcorr, -1)
 
-    def triangulate_proj(self, lcorr, rcorr, proj1, proj2):
+    def triangulate_pose(self, lcorr, rcorr, pose_index):
 
         img_points1_cv = np.array([[lcorr]], dtype=np.float32)
         img_points2_cv = np.array([[rcorr]], dtype=np.float32)
@@ -55,8 +56,12 @@ class Calibration:
 
         img_point1 = img_points1_cv[0,0]
         img_point2 = img_points2_cv[0,0]
+
+        p1 = lib.get_projection_matrix(self.mtx1, self.rvecs1[pose_index], self.tvecs1[pose_index])
+        p2 = lib.get_projection_matrix(self.mtx2, self.rvecs2[pose_index], self.tvecs2[pose_index])
+
         obj_point_reconstructed = lib.triangulate_from_image_points(img_point1, img_point2,
-                                    proj1, proj2)
+                                    p1, p2)
 
         return obj_point_reconstructed + self.offset # np.array([x,y,z])
 
@@ -91,19 +96,11 @@ class Calibration:
                                                                         flags=my_flags,
                                                                         criteria=CRIT)
 
-        # select LAST extrinsics for projection matrices
-        self.rvec1 = rvecs1[-1]
-        self.tvec1 = tvecs1[-1]
-        self.rvec2 = rvecs2[-1]
-        self.tvec2 = tvecs2[-1]
-
-        # calculate projection matrices
-        self.projs1 = []
-        self.projs2 = []
-        for r1,t1,r2,t2 in zip(rvecs1, tvecs1, rvecs2, tvecs2):
-            self.projs1.append(lib.get_projection_matrix(mtx1, r1, t1))
-            self.projs2.append(lib.get_projection_matrix(mtx2, r2, t2))
-
+        # select calibration parameters
+        self.rvecs1 = rvecs1
+        self.tvecs1 = tvecs1
+        self.rvecs2 = rvecs2
+        self.tvecs2 = tvecs2
         self.mtx1 = mtx1
         self.mtx2 = mtx2
         self.dist1 = dist1
@@ -124,15 +121,16 @@ class Calibration:
                 op = self.obj_points[i,j,:]
                 ip1 = self.img_points1[i,j,:]
                 ip2 = self.img_points2[i,j,:]
-                op_recon = self.triangulate_proj(ip1,ip2,self.projs1[i], self.projs2[i])
+                op_recon = self.triangulate_pose(ip1,ip2, i)
                 diff = op - op_recon
                 diffs.append(diff)
         self.diffs = np.array(diffs, dtype=np.float32)
         self.mean_error = np.mean(self.diffs, axis=0)
         self.std_error = np.std(self.diffs, axis=0)
-        self.rmse_tri = np.sqrt(np.mean(self.diffs * self.diffs, axis=0))
+        #self.rmse_tri = np.sqrt(np.mean(self.diffs * self.diffs, axis=0))
+        self.rmse_tri = np.sqrt(np.mean(self.diffs * self.diffs))
         # RMS error from triangulation (in um)
-        self.rmse_tri_norm = np.linalg.norm(self.rmse_tri)
+        #self.rmse_tri_norm = np.linalg.norm(self.rmse_tri)
 
 
     def calibrate_stereo(self, img_points1, img_points2, obj_points):
@@ -166,48 +164,29 @@ class Calibration:
                                                                         flags=my_flags,
                                                                         criteria=CRIT)
 
-        # save all the extrinsics for reconstruction later
-        self.rvecs1 = rvecs1
-        self.tvecs1 = tvecs1
-        self.rvecs2 = rvecs2
-        self.tvecs2 = tvecs2
-
         stereocalibration_flags = cv2.CALIB_FIX_INTRINSIC
         rmse_stereo, _, _, _, _, R, T, E, F = cv2.stereoCalibrate(obj_points, img_points1, 
                                                                     img_points2, mtx1, dist1,
                                                                      mtx2, dist2, (WF, HF),
                                                                     criteria = CRIT,
                                                                     flags = stereocalibration_flags)
-        self.R = R
-        self.T = T
-        self.E = E
-        self.F = F
 
 
-        """ Don't actually need these
-        #RT matrix for C1 is identity.
-        RT1 = np.concatenate([np.eye(3), [[0],[0],[0]]], axis = -1)
-        self.P1 = mtx1 @ RT1 #projection matrix for C1
-        #RT matrix for C2 is the R and T obtained from stereo calibration.
-        RT2 = np.concatenate([R, T], axis = -1)
-        self.P2 = mtx2 @ RT2 #projection matrix for C2
-        """
-
-        # calculate projection matrices
-        """
-        self.projs1 = []
-        self.projs2 = []
-        for r1,t1,r2,t2 in zip(rvecs1, tvecs1, rvecs2, tvecs2):
-            self.projs1.append(lib.get_projection_matrix(mtx1, r1, t1))
-            self.projs2.append(lib.get_projection_matrix(mtx2, r2, t2))
-        """
-
+        # save all the calibration parameters
+        self.rvecs1 = rvecs1
+        self.tvecs1 = tvecs1
+        self.rvecs2 = rvecs2
+        self.tvecs2 = tvecs2
         self.mtx1 = mtx1
         self.mtx2 = mtx2
         self.dist1 = dist1
         self.dist2 = dist2
         self.rmse_reproj_1 = rmse1  # RMS error from reprojection (in pixels)
         self.rmse_reproj_2 = rmse2
+        self.R = R
+        self.T = T
+        self.E = E
+        self.F = F
 
         # save calibration points
         self.obj_points = obj_points
@@ -221,20 +200,13 @@ class Calibration:
                 op = self.obj_points[i,j,:]
                 ip1 = self.img_points1[i,j,:]
                 ip2 = self.img_points2[i,j,:]
-                #op_recon = self.triangulate_proj(ip1,ip2,self.projs1[i], self.projs2[i])
                 op_recon = self.triangulate_stereo_pose(ip1,ip2, i)
                 diff = op - op_recon
                 diffs.append(diff)
         self.diffs = np.array(diffs, dtype=np.float32)
         self.mean_error = np.mean(self.diffs, axis=0)
         self.std_error = np.std(self.diffs, axis=0)
-        self.rmse_tri = np.sqrt(np.mean(self.diffs * self.diffs, axis=0))
-        # RMS error from triangulation (in um)
-        self.rmse_tri_norm = np.linalg.norm(self.rmse_tri)
-
-        print('rmse1 = ', rmse1)
-        print('rmse2 = ', rmse2)
-        print('rmse_stereo = ', rmse_stereo)
+        self.rmse_tri = np.sqrt(np.mean(self.diffs * self.diffs))
 
 
     def triangulate_stereo(self, lcorr, rcorr):
@@ -247,8 +219,8 @@ class Calibration:
         rvec1 = self.rvecs1[pose_index]
         tvec1 = self.tvecs1[pose_index]
 
-        img_points1_cv = np.array([[lcorr]], dtype=np.float32)
-        img_points2_cv = np.array([[rcorr]], dtype=np.float32)
+        img_points1_cv = np.array([lcorr], dtype=np.float32)
+        img_points2_cv = np.array([rcorr], dtype=np.float32)
 
         # undistort
         img_points1_cv = lib.undistort_image_points(img_points1_cv, self.mtx1, self.dist1)
@@ -269,54 +241,5 @@ class Calibration:
                                     pp1, pp2)
 
         return obj_point_reconstructed + self.offset # np.array([x,y,z])
-
-
-    def triangulate_cv(self, lcorr, rcorr):
-
-        img_points1_cv = np.array([[lcorr]], dtype=np.float32)
-        img_points2_cv = np.array([[rcorr]], dtype=np.float32)
-
-        # undistort
-        img_points1_cv = lib.undistort_image_points(img_points1_cv, self.mtx1, self.dist1)
-        img_points2_cv = lib.undistort_image_points(img_points2_cv, self.mtx2, self.dist2)
-
-        thing1 = np.array([lcorr], dtype=np.float32).T
-        thing2 = np.array([rcorr], dtype=np.float32).T
-        proj1 = self.projs1[-1]
-        print('shaep = ', proj1.shape)
-        proj2 = self.projs2[-1]
-        obj_point_reconstructed = cv2.triangulatePoints(proj1, proj2, img_points1_cv, img_points2_cv)
-
-        return obj_point_reconstructed + self.offset # np.array([x,y,z])
-
-
-    def triangulate_temuge(self, lcorr, rcorr):
-
-        img_points1_cv = np.array([[lcorr]], dtype=np.float32)
-        img_points2_cv = np.array([[rcorr]], dtype=np.float32)
-
-        # undistort
-        img_points1_cv = lib.undistort_image_points(img_points1_cv, self.mtx1, self.dist1)
-        img_points2_cv = lib.undistort_image_points(img_points2_cv, self.mtx2, self.dist2)
-
-        img_point1 = img_points1_cv[0,0]
-        img_point2 = img_points2_cv[0,0]
-
-        #RT matrix for C1 is identity.
-        RT1 = np.concatenate([np.eye(3), [[0],[0],[0]]], axis = -1)
-        P1 = self.mtx1 @ RT1 #projection matrix for C1
-     
-        #RT matrix for C2 is the R and T obtained from stereo calibration.
-        RT2 = np.concatenate([self.R, self.T], axis = -1)
-        P2 = self.mtx2 @ RT2 #projection matrix for C2
-
-        opt_recon = lib.triangulate_from_image_points(img_point1, img_point2, P1, P2)
-        rti = lib.get_inverse_rt_matrix(self.rvec1, self.tvec1)
-        opt_recon_homo = np.zeros(4)
-        opt_recon_homo[:3] = opt_recon
-        opt_recon_homo[3] = 1
-        opt_recon = np.matmul(rti, opt_recon_homo)
-
-        return opt_recon + self.offset # np.array([x,y,z])
 
 
