9af9ef18434556fa2ed90601ceaa8629c4522a3b
hannalee2 <hanna.lee@alleninstitute.org>
2026-02-25 14:43:51 -0800

    Update camera setting signals
    Gui changes sync with camera (HW) and model

--- parallax/screens/screen_setting.py
+++ parallax/screens/screen_setting.py
@@ -30,11 +30,12 @@ class ScreenSetting(QWidget):
         self.settingButton = self._get_setting_button(self.parent)  # UI - Button
         self.settingMenu = self._get_setting_menu(self.parent)  # UI - Menu
         self._setup_settingMenu()  # Singal connections and initial values
-        self._update_ui_from_model() # Initial sync to ensure UI reflects current model state
+        #self._update_ui_from_model() # model --> Gui on init
         self.settingButton.toggled.connect(self._handle_menu_toggle)
 
     def _handle_menu_toggle(self, is_checked):
         if is_checked:
+            # TODO - open (Disk -> Camera & Model & GUI sync)
             self._update_ui_from_model()
             # Position logic
             btn_pos = self.settingButton.mapToGlobal(QPoint(0, 0))
@@ -43,6 +44,8 @@ class ScreenSetting(QWidget):
                                   btn_pos.y() - self.settingMenu.height() - parent_pos.y())
             self.settingMenu.show()
         else:
+            # TODO - close (Model -> Disk) 
+            # During Change, Model sync to Camera in real-time, so no need to sync on close)
             self.settingMenu.hide()
             # Final sync and Save to disk
             self._sync_ui_to_model()
@@ -50,11 +53,14 @@ class ScreenSetting(QWidget):
 
     def _update_ui_from_model(self):
         """Syncs GUI to the current Model state."""
-        if not self.camera_setting: return
+        if not self.camera_setting:
+            return
+        # fps
         self.settingMenu.fpsSlider.blockSignals(True)
         self.settingMenu.fpsSlider.setValue(int(self.camera_setting.fps))
         self.settingMenu.fpsNum.setNum(int(self.camera_setting.fps))
         self.settingMenu.fpsSlider.blockSignals(False)
+
         self.settingMenu.customName.setText(self.camera_setting.customName)
         # TODO
 
@@ -68,45 +74,91 @@ class ScreenSetting(QWidget):
         self.camera_setting.gamma = int(self.settingMenu.gammaSlider.value())
 
     def _setup_settingMenu(self):
+        # Should sync "GUI & camera(HW) & model state"
         self._setup_sn()
         self._setup_custom_name()
         self._setup_framerate()
         self._setup_exposure()
-        #self._setup_gain()
-        #self._setup_gamma()
-        #self._sedtup_white_balance()
-        #self._setup_color_channel()
+        self._setup_gain()
+        self._setup_gamma()
+        #self._setup_gamma_auto()
+        self._setup_white_balance_auto()
+        self._setup_color_channel()
 
     def _setup_sn(self):
-        self.settingMenu.snLabel.setText(self.sn)
+        self.camera_setting.customName = self.sn  # update model
+        self.settingMenu.snLabel.setText(self.sn)  # update GUI
 
     def _setup_custom_name(self):
-        # Update gui when user types
-        self.settingMenu.customName.textChanged.connect(
-            lambda text: self._update_groupbox_name(self.parent, text)
-        )
+        def on_name_changed(text):
+            self.camera_setting.customName = text  # update model
+            self._update_groupbox_name(self.parent, text)  # update GUI
+        self.settingMenu.customName.textChanged.connect(on_name_changed)
 
     def _setup_framerate(self):
         def on_sync():
             val = self.settingMenu.fpsSlider.value()
-            self.screen.set_camera_setting("fps", val)
-            self.camera_setting.fps = float(val)
-            # Bottleneck check
-            actual = self.screen.get_camera_setting("fps")
-            self.settingMenu.fpsNum.setStyleSheet("color: red;" if actual < val - 0.5 else "color: white;")
-
-        self.settingMenu.fpsSlider.valueChanged.connect(lambda v: self.settingMenu.fpsNum.setNum(v))
+            self.screen.set_camera_setting("fps", val)  # Update camera
+            actual_val = self.screen.get_camera_setting("fps")
+            # Update the actual fps value from camera
+            self.camera_setting.fps = float(actual_val)  # Update model
+            # Update slider to actual value. User can see actual fps value from slider.
+            self.settingMenu.fpsSlider.setValue(int(actual_val)) # Update GUI
+        self.settingMenu.fpsSlider.valueChanged.connect(lambda v: self.settingMenu.fpsNum.setNum(v)) # Update GUI
+        # Due to the HW delay, we only sync on slider release.
         self.settingMenu.fpsSlider.sliderReleased.connect(on_sync)
+
+    def _setup_gain(self):
+        def on_sync():
+            val = self.settingMenu.gainSlider.value()
+            self.screen.set_camera_setting("gain", val)  # Update Camera
+            self.camera_setting.gain = float(val)  # Update model
+            self.settingMenu.gainNum.setNum(val)  # Update GUI
+        self.settingMenu.gainSlider.valueChanged.connect(on_sync)
+
+    def _setup_gamma(self):
+        def on_sync():
+            val = self.settingMenu.gammaSlider.value()
+            self.screen.set_camera_setting("gamma", val)  # Update Camera
+            self.camera_setting.gamma = int(val)  # Update model
+            self.settingMenu.gammaNum.setNum(val)  # Update GUI
+        self.settingMenu.gainSlider.valueChanged.connect(on_sync)
+
+    def _setup_white_balance_auto(self):
+        settingLayout = self.settingMenu.layout()
+        settingLayout.addWidget(self.settingMenu.wbAuto, 10, 1, 2, 1)
+        # TODO Setup actual W/B controls and sync
+
+    def _setup_color_channel(self):
+        def on_sync_blue():
+            val = self.settingMenu.wbSliderBlue.value()
+            self.screen.set_camera_setting("wbBlue", val)  # Update Camera
+            self.camera_setting.wbBlue = int(val)  # Update model
+            self.settingMenu.wbSliderBlue.setNum(val)  # Update GUI
+        def on_sync_red():
+            val = self.settingMenu.wbSliderRed.value()
+            self.screen.set_camera_setting("wbRed", val)  # Update Camera
+            self.camera_setting.wbRed = int(val)  # Update model
+            self.settingMenu.wbSliderRed.setNum(val)  # Update GUI
+        self.settingMenu.wbSliderBlue.valueChanged.connect(on_sync_blue)
+        self.settingMenu.wbSliderRed.valueChanged.connect(on_sync_red)
+
+    def _setup_white_balance(self):
+        def on_sync():
+            val = self.settingMenu.gammaSlider.value()
+            self.screen.set_camera_setting("gamma", val)
+            self.camera_setting.gamma = int(val)
+        self.settingMenu.gammaSlider.valueChanged.connect(lambda v: self.settingMenu.gammaNum.setNum(v))
+        self.settingMenu.gainSlider.valueChanged.connect(on_sync)
 
     def _setup_exposure(self):
         def on_sync():
             val = self.settingMenu.expSlider.value()
-            self.screen.set_camera_setting("exposure", val * 1000)
-            self.camera_setting.exposureTime_ms = float(val)
-            self.settingMenu.expNum.setNum(val)
-
-        self.settingMenu.expSlider.sliderReleased.connect(on_sync)
-        self.settingMenu.expAuto.clicked.connect(self._toggle_exposure_auto)
+            self.screen.set_camera_setting("exposure", val * 1000)  # Update Camera
+            self.camera_setting.exposureTime_ms = float(val)  # Update model
+            self.settingMenu.expNum.setNum(val)  # Update GUI
+        self.settingMenu.expSlider.valueChanged.connect(on_sync)
+        #self.settingMenu.expAuto.clicked.connect(self._toggle_exposure_auto)
 
     def _toggle_exposure_auto(self):
         current = self.camera_setting.exposureAuto
