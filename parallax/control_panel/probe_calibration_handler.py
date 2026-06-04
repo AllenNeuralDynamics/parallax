74b2d83acd4e21106c1a83c1e412381b7756a8f3
hannalee2 <hanna.lee@alleninstitute.org>
2026-06-04 11:49:25 -0700

    Fix bug in clearing calib status for all stages

--- parallax/control_panel/probe_calibration_handler.py
+++ parallax/control_panel/probe_calibration_handler.py
@@ -468,8 +468,8 @@ class ProbeCalibrationHandler(QWidget):
             self.clearRequested.emit(self.selected_stage_id)
             # update global coords. Set  to '-' on UI
         else:  # Reset all probel calibration status
-            for sn in self.model.get_list_of_stage_sns():
-                self.clearRequested.emit(self.selected_stage_id)
+            for stage_sn in self.model.get_list_of_stage_sns():
+                self.clearRequested.emit(stage_sn)
 
         # Set as Uncalibrated
         self.calculator.set_calc_functions()
